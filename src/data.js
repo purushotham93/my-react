584b05152b21531b136f9dec45a71caae76e94ff
purushotham93 <62345144+purushotham93@users.noreply.github.com>
2024-05-09 14:50:47 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const videoDB = [{
+    id:1,
+    title: 'React JS tutorial',
+    views: '999K',
+    time: '1 year ago',
+    channel: 'Purushotham react',
+    verified: true
+  },{
+    id:2,  
+    title: 'Node JS tutorial',
+    views: '100K',
+    time: '1 year ago',
+    channel: 'Purushotham react',
+    verified: false
+  },
+  { id:3,
+    title: 'MongoDb JS tutorial',
+    views: '1M',
+    time: '1 month ago',
+    channel: 'Purushotham react',
+    verified: true
+  }];
+
+export  {videoDB};
