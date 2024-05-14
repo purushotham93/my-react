5b6c18ea1cc3eb8a2d596f16a64c974beaed24d6
purushotham93 <62345144+purushotham93@users.noreply.github.com>
2024-05-14 12:19:16 +0530

    Update Video.js

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,27 +1,27 @@
 import './Video.css';
 
-function Video({title,id,channel="Purushotham react",views,time,verified,children,deleteVideo, editVideo}) {
+function Video({ title, id, channel = "Purushotham react", views, time, verified, children, dispatch, editVideo }) {
   console.log('render Video')
 
-  
+
   return (
-      <>
+    <>
       <div className='container'>
-        <button className='close' onClick={() => deleteVideo(id)}>X</button>
+        <button className='close' onClick={() => dispatch({ type: 'DELETE', payload: { id } })}>X</button>
         <button className='edit' onClick={() => editVideo(id)}>Edit</button>
-      <div className="pic">
-      <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />
+        <div className="pic">
+          <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />
+        </div>
+        <div className="title">{title}</div>
+        <div className="channel">{channel} {verified && '✅'} </div>
+        <div className="views">
+          {views} views <span>.</span> {time}
+        </div>
+        <div>
+          {children}
+        </div>
       </div>
-      <div className="title">{title}</div>
-      <div className="channel">{channel} {verified && '✅'} </div>
-      <div className="views">
-        {views} views <span>.</span> {time}
-      </div>
-      <div>
-        {children}
-      </div>
-      </div>
-      </>
+    </>
   );
 }
 
